47ef60bff138d3909193ff5857e7161b214be2de
Avinash <avinashkumar_arvind@srmuniv.edu.in>
2019-09-24 22:37:40 +0530

    updating a shop page, collection previev and collection compon comp.

--- src/App.js
+++ src/App.js
@@ -2,20 +2,17 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import {Route,Switch} from 'react-router-dom'
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () =>(
-  <div>
-  <h1> HATS PAGE</h1>
-  </div>
-)
+
 
 function App() {
   return (
     <div >
-    <Switch>
-    <Route exact={true} path='/' component={HomePage}></Route>
-   <Route exact ={true} path='/hats' component={HatsPage}></Route>
-    </Switch>
+        <Switch>
+             <Route exact={true} path='/' component={HomePage}></Route>
+             <Route exact ={true} path='/shop' component={ShopPage}></Route>
+         </Switch>
    
     </div>
   );
