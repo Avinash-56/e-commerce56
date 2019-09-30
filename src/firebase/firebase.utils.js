f142e63e5d7210690340796b6cdfd7ecbb550f9f
Avinash <avinashkumar_arvind@srmuniv.edu.in>
2019-09-30 22:39:02 +0530

    implemented firebase utils and included ability for signup with email and password

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import  'firebase/firestore'
 import 'firebase/auth'
+import { async } from 'q'
 
 const config = {
     apiKey: "AIzaSyD6Gtzs-e5ng6gabY525ens2zjqHWzADcg",
@@ -12,6 +13,34 @@ const config = {
     appId: "1:451371214971:web:c3fb70f49ab2ae5fe9dade",
     measurementId: "G-C9PG7SKHPF"
 }
+
+export const createUserProfileDocument = async (userAuth, additionalData) =>{
+    if(!userAuth) return
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`)
+
+    const snapShot =await userRef.get()
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth
+        const createdAt = new Date()
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message)
+        }
+    }
+   
+    return userRef
+
+}
+
 
 firebase.initializeApp(config)
 
