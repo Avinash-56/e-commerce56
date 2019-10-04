541bef5123030d809efc819b8ea605462668bf05
Avinash <avinashkumar_arvind@srmuniv.edu.in>
2019-10-04 19:01:01 +0530

    added addItem and removeItem to Checkout Page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -9,3 +9,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) =>{
 
         return [...cartItems,{...cartItemToAdd, quantity:1}]
 }
+
+export const  removeItemFromCart = (cartItems, cartItemToRemove) =>{
+      const existingCardItem = cartItems.find(
+          cartItem => cartItem.id === cartItemToRemove.id
+      ) 
+
+      if(existingCardItem.quantity===1){
+          return cartItems.filter(cartItem => cartItem.id !==cartItemToRemove)
+      }
+
+      return cartItems.map(
+          cartItem=>cartItem.id === cartItemToRemove.id ?
+          {...cartItem, quantity: cartItem.quantity-1}:
+          cartItem
+      )
+}
