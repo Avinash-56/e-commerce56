2afb39a26f710b1be27fd4739ccb64c893f6b874
Avinash <avinashkumar_arvind@srmuniv.edu.in>
2019-10-09 21:31:02 +0530

    moved our directory and shop data into its own reduer , flow for directory/shop

---
+++ src/components/collections-overview/collections-overview.component.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import  {connect} from 'react-redux'
+import {createStructuredSelector} from 'reselect'
+import './collections-overview.styles.scss'
+import CollectionPreview from '../collection-preview/collection-preview.component'
+import  {selectCollectionForPreview} from '../../redux/shop/shop.selector'
+
+const CollectionsOverview  =({collections}) =>(
+    <div className='collections-overview'>
+    {
+        collections.map(({id, ...otherCollectionPreview})=>(
+        <CollectionPreview key={id} 
+        {...otherCollectionPreview}></CollectionPreview>))
+    }
+    </div>
+)
+
+const mapStateToProps = createStructuredSelector({
+    collections: selectCollectionForPreview
+}) 
+
+export default connect(mapStateToProps)(CollectionsOverview)
